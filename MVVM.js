7032acacd0a9fe0b0d4a889a7edc40999bad2776
unknown <1038747706@qq.com>
2020-06-07 23:29:51 +0800

    增加一些东西

---
+++ MVVM.js
@@ -0,0 +1,49 @@
+class Compiler {
+    constructor(el, vm) {
+        this.el = this.isElementNode(el) ? el : document.querySelector(el);
+        this.vm = vm;
+        let fragment = this.node2fragment(this.el);
+        this.compile(fragment);
+        this.el.appendChild(fragment);
+    }
+    compile(node) {
+        let childNodes = node.childNodes;
+        [...childNodes].forEach((child) => {
+            if (this.isElementNode(child)) { } else { }
+        })
+    }
+    isDirective(attrName) {
+        return attrName.startsWith('v-');
+    }
+    compileElement(node) {
+        let attributes = node.attributes;
+        [...attributes].forEach((attr) => {
+            let { name, value } = attr;
+            if (this.isDirective(name)) { }
+        })
+    }
+    compileText(node) {
+        let content = node.textContent;
+    }
+    node2fragment(node) {
+        let fragment = document.createDocumentFragment();
+        let firstChild;
+        while (firstChild = node) {
+            fragment.append(firstChild);
+        }
+        return fragment;
+    }
+    isElementNode(node) {
+        return node.nodeType === 1;
+    }
+}
+
+class Vue {
+    constructor(options) {
+        this.$el = options.el;
+        this.$data = options.data;
+        if (this.$el) {
+            new Compiler(this.$el, this);
+        }
+    }
+}
