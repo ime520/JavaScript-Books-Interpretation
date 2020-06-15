5a5354c28359a17e62d9757265c0fe5fccf58e25
lsz <liusz@ddd.online>
2020-06-15 17:58:03 +0800

    微调

--- test.js
+++ test.js
@@ -1,25 +1,19 @@
-var watcher = {
-  subs: {},
-  subscribe(type, fn) {
-    if (!this.subs[type]) {
-      this.subs[type] = [];
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function () {
+    var slice = [].slice;
+    var func = this;
+    var fNOP = function () {};
+    var outerArgs = slice.call(arguments, 1);
+    var newFunc = function () {
+      var innerArgs = slice.call(arguments);
+      var fullArgs = outerArgs.concat(innerArgs);
+      return func.call(this, ...fullArgs);
+    };
+    // 如果是函数
+    if (this.prototype) {
+      fNOP.prototype = this.prototype;
     }
-    this.subs[type].push(fn);
-  },
-  unsubscriber(type, fn) {
-    this.subs[type] = this.subs[type].filter((func) => {
-      return func !== fn;
-    });
-  },
-  publish(type, ...args) {
-    this.subs[type].forEach((func) => {
-      func(...args);
-    });
-  },
-};
-
-var tom = {
-  read(info) {
-    console.log(info);
-  },
-};
+    newFunc.prototype = new fNOP();
+    return newFunc;
+  };
+}
